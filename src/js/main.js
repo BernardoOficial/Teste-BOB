f7e72847024dbe5d3afbeeacc909ab7e09d6603b
Bernardo Pereira Oliveira <bernardo.pereira@foster.com.br>
2021-04-15 19:04:00 -0300

    Criando a página de produto e componentes

--- src/js/main.js
+++ src/js/main.js
@@ -78,3 +78,39 @@ console.log("Apareci");
 // };
 
 // btnTheme.addEventListener("click", switchTheme);
+
+//////////////////////////////////////////////
+
+// ACCORDION
+const accordions = Array.from(document.querySelectorAll(".main__accordion-item"));
+
+accordions.forEach(accordion => {
+
+  let isActive = false;
+  const accordionTitle = accordion.querySelector('.main__title');
+  
+  accordionTitle.addEventListener('click', handleClick)
+
+  function handleClick(evento) {
+    evento.stopPropagation();
+
+    isActive = !isActive;
+
+    accordionTitle.classList.toggle("main__title--active");
+    
+    const currentTitleHeight = accordionTitle.offsetHeight;
+    const currentParagraphHeight = accordionTitle.nextElementSibling.offsetHeight;
+
+    isActive
+      ? (accordion.style.height = `${
+          Number(currentTitleHeight) + Number(currentParagraphHeight)
+        }px`)
+      : (accordion.style.height = `${currentTitleHeight}px`);
+
+  }
+})
+
+// Produtos
+fetch("http://localhost:3002/products")
+  .then(response => response.json())
+  .then(products => console.log(products))
